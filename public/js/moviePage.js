78fa7e4f9e1831c00e9389bb31d9d510b392b066
ike barranco <barranco.ike@outlook.com>
2020-06-14 19:42:21 -0600

    Added API get for OMBD to populate searched movie. Also added click event for button to add to favourites. JS page has been added and intergrated into the file structure as well

---
+++ public/js/moviePage.js
@@ -0,0 +1,59 @@
+$(document).ready(() => {
+  $("#searchBtn").on("click", event => {
+    event.preventDefault();
+    const title = $("#search").val().trim();
+    const queryURL = "https://www.omdbapi.com/?t=" + title + "&apikey=cd2db0c6";
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(data => {
+      $("#resultRow").html("");
+      const resultDiv = $("<div>");
+      resultDiv.addClass("card");
+      $("#resultRow").append(resultDiv);
+
+      const resultCard = $("<div>");
+      resultCard.addClass("card-body");
+      resultDiv.append(resultCard);
+
+      const resultCardImage = $("<img>");
+      resultCardImage.attr("src", data.Poster);
+      resultDiv.append(resultCardImage);
+
+      const resultCardTitle = $("<p>");
+      resultCardTitle.text(`${data.Title}`);
+      resultCardTitle.addClass("card-title");
+      resultDiv.append(resultCardTitle);
+
+      const resultCardYear = $("<p>");
+      resultCardYear.text(`Releaseed Year: ${data.Year}`);
+      resultCardTitle.addClass("card-text");
+      resultDiv.append(resultCardYear);
+
+      const resultCardGenre = $("<p>");
+      resultCardGenre.text(`Genre(s): ${data.Genre}`);
+      resultCardTitle.addClass("card-text");
+      resultDiv.append(resultCardGenre);
+
+      const resultCardPlot = $("<p>");
+      resultCardPlot.text(`Plot: ${data.Plot}`);
+      resultCardTitle.addClass("card-text");
+      resultDiv.append(resultCardPlot);
+
+      const favouriteButton = $("<button>");
+      favouriteButton.text("Add To Favourites");
+      favouriteButton.addClass("cardButton");
+      favouriteButton.attr("id", "favBtn");
+      resultDiv.append(favouriteButton);
+
+      console.log(data);
+    });
+  });
+  $("#resultRow").on("click", "button", event => {
+    event.preventDefault();
+    console.log("this");
+  });
+});
+
+//
+// let queryURL = "https://www.omdbapi.com/?t=" + title + "&apikey=a29b2a0f";
