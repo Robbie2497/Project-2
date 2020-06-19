089d8ea16b64eb36d40e7f37264e92135836e89b
ike barranco <barranco.ike@outlook.com>
2020-06-18 19:25:39 -0600

    Comments added to movie.js

--- config/seed.sql
+++ config/seed.sql
@@ -1,3 +1,4 @@
+-- Use movies Database
 USE movies_db;
-
+-- Show All Movies
 SELECT * FROM Movies;
--- models/movies.js
+++ models/movies.js
@@ -1,5 +1,10 @@
+// Export sequilize function
 module.exports = function(sequelize, DataTypes) {
+  //Create Movie Table
   const Movie = sequelize.define("Movie", {
+    //title For The Movie
+    //Data Type: String
+    //Limitations: Can't be null, must have at least one character
     title: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -7,16 +12,28 @@ module.exports = function(sequelize, DataTypes) {
         len: [1]
       }
     },
+    //favourite Open For Future Dev
+    //Data Type: Boolean
+    //Limitations: default is false
     favorite: {
       type: DataTypes.BOOLEAN,
       default: false
     },
+    //reason for save reason
+    //Data Type: String
+    //Limitations: None
     reason: {
       type: DataTypes.STRING
     },
+    //Rating For Movie Rating
+    //Data Type: Integer
+    //Limitations: None
     rating: {
       type: DataTypes.INTEGER
     },
+    //Year that movie was released
+    //Data Type: String
+    //Limitations: None
     year: {
       type: DataTypes.STRING
     }
